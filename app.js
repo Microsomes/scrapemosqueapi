96fa8cdaee950433578dfeecf66a71d768acb6d4
Microsomes <tayyabdev@outlook.com>
2020-08-27 22:44:05 +0100

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+var scrape= require("./modules/scrapeNamaz");
+var cors= require("cors");
+const ExpressCache = require('express-cache-middleware')
+const cacheManager = require('cache-manager') 
+const cacheMiddleware = new ExpressCache(
+    cacheManager.caching({
+        store: 'memory', max: 10000, ttl: 36000
+    })
+)
+const express= require("express");
+const app= express();
+cacheMiddleware.attach(app)
+app.use(cors());
+app.get("/namaz",async (req,res)=>{
+    res.status(200).json({
+        localtion:"Birmingham United Kingdom",
+        source:"https://centralmosque.org.uk/mobile-timetable/",
+        times:await scrape("one")
+    })
+});
+
+app.listen("99")
---
+++ modules/scrapeNamaz.js
@@ -0,0 +1,46 @@
+const puppeteer = require('puppeteer');
+async function scrapeNamaz(which){
+    const browser = await puppeteer.launch({headless:true,args: ['--no-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.goto('https://centralmosque.org.uk/mobile-timetable/');
+    await page.waitFor(2000);
+    //begin scrapping
+    var allPrayers= await page.evaluate(()=>{
+        var allP= document.querySelectorAll(".prayer-start");
+        var toReturn=[]
+        toReturn.push({
+            name:"farj",
+            value:allP[0].innerText,
+            englishName:"first prayer",
+            englishSmall:"first"
+        })
+        toReturn.push({
+            name:"zhur",
+            value:allP[2].innerText,
+            englishName:"second prayer",
+            englishSmall:"second"
+        })
+        toReturn.push({
+            name:"asr",
+            value:allP[3].innerText,
+            englishName:"third prayer",
+            englishSmall:"third"
+        })
+        toReturn.push({
+            name:"maghrib",
+            value:allP[4].innerText,
+            englishName:"fourth prayer",
+            englishSmall:"fourth"
+        })
+        toReturn.push({
+            name:"isha",
+            value:allP[5].innerText,
+            englishName:"fifth prayer",
+            englishSmall:"fifth"
+        })
+        return toReturn;
+    })
+    return allPrayers;
+}
+module.exports= scrapeNamaz;
